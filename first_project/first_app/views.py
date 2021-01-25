22b75f014936becd9ec24c8ea6d50c616b1625ed
Mark Carlebach <markjcarlebach@gmail.com>
2021-01-25 15:34:49 -0500

    dun with lesson 118

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# This index is a "view"
+def index(request):
+
+    # Views must return HttpResponse object
+    return HttpResponse("Hello World")
