fc7f5673c7495aa498f17e5590a03cb3d3c1592b
Mark Carlebach <markjcarlebach@gmail.com>
2021-01-28 07:52:52 -0500

    dun with level II

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# STEP1:  Import models
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 
 # This index is a "view"
 def index(request):
 
-    # Views must return HttpResponse object
-    my_dict = {'insert_me':  "now I am coming from first_app/index.html"}
-    return render(request, "first_app/index.html", context=my_dict)
+    # STEP 2:  Get the data from the model
+    webpages_list = AccessRecord.objects.order_by("date")
+    date_dict = {"access_records":  webpages_list}
+
+    # STEP 3:  Pass the data to the template
+    return render(request, "first_app/index.html", context=date_dict)
